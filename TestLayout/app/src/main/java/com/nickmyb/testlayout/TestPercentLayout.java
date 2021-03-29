f10fd54208111fdba564506dacd64efcc920e0ef
nickmyb <nick.yangbin.ma@gmail.com>
2021-03-29 10:43:21 +0800

    ci: 第3章 - 软件也要拼脸蛋--UI开发的点点滴滴
    基本布局, 自定义布局和控件

---
+++ TestLayout/app/src/main/java/com/nickmyb/testlayout/TestPercentLayout.java
@@ -0,0 +1,14 @@
+package com.nickmyb.testlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestPercentLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_percent_layout);
+    }
+}
---
+++ TestLayout/app/src/main/java/com/nickmyb/testlayout/TestRelativeLayout.java
@@ -0,0 +1,14 @@
+package com.nickmyb.testlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestRelativeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_relative_layout);
+    }
+}
